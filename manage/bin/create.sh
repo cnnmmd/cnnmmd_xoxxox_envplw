345ef699fecc1d9ee8a0f74537f6dd8ebab44a03
azsgit <10238943+azsgit@users.noreply.github.com>
2026-07-06 02:16:21 +0900

    add initial files

---
+++ manage/bin/create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pthtop="$(cd "$(dirname "${0}")/../../../.." && pwd)"
+source "${pthtop}"/manage/lib/params.sh
+source "${pthtop}"/manage/lib/shared.sh
+source "${pthcrr}"/params.sh
+
+pthapp="${pthsrc}"/envplw
+pthprj="${pthapp}/plw"
+cntprj='/opt/playwright'
+
+test -d "${pthapp}" || mkdir "${pthapp}"
+test -d "${pthprj}" || mkdir "${pthprj}"
+
+addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
+
+docker run -v "${pthprj}":"${cntprj}" --name ${cnttgt} ${imgtgt} /exp/runcfg.sh "${cntprj}" && \
+docker commit ${cnttgt} ${imgtgt} && \
+docker stop ${cnttgt} && \
+docker rm ${cnttgt}
